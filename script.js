0c7928ad1be984477c124b853d60301a6765dee1
unknown <lamichhane.kiran11@gmail.com>
2023-12-27 18:57:05 +0545

    final working-pip

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+const videoElement =document.getElementById('video');
+const button =document.getElementById('button');
+
+// prompt the selection of media stream , passs to element then play  
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject =mediaStream;
+       videoElement.onloadedmetadata =()=>{
+        videoElement.play();
+       }
+    }catch(error)
+{
+   
+}
+}
+button.addEventListener('click', async()=>{
+// disable when clicked
+button.disabled = true;
+
+// PIP start
+await videoElement.requestPictureInPicture();
+ 
+// reset button 
+button.disabled =false;
+
+});
+
+// on load
+selectMediaStream();
